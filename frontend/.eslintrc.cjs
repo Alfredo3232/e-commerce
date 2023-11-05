b7238db54bd567bd0e58031ef33f818fbd079a00
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-11-05 11:47:16 -0800

    Initialized repository

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,60 @@
+module.exports = {
+  root: true,
+  env: {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "arrow-body-style": [
+      "error",
+      "as-needed"
+    ],
+    "key-spacing": ["error", {
+      "align": {
+        "afterColon": true,
+        "beforeColon": true,
+        "on": "colon"
+      }
+    }],
+    "no-console": "warn",
+    "no-debugger": "error",
+    "no-dupe-keys": "error",
+    "no-duplicate-imports": "error",
+    "no-empty": "error",
+    "no-trailing-spaces": "error",
+    "no-unused-vars": "warn",
+    "no-var": "error",
+    "object-curly-newline": ["error", {
+      "ExportDeclaration": { "consistent": true, "minProperties": 3 },
+      "ImportDeclaration": { "consistent": true, "minProperties": 3 },
+      "ObjectExpression": { "consistent": true, "minProperties": 3 },
+      "ObjectPattern": { "consistent": true, "minProperties": 3 }
+    }],
+    "quotes": [
+      "error",
+      "double",
+      {
+        "allowTemplateLiterals": true,
+        "avoidEscape": true
+      }
+    ],
+    "semi": ["error", "always"],
+    "eqeqeq": ["error", "smart"],
+    "no-else-return": "error"
+  },
+}
