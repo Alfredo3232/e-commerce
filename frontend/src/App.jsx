369279abf4cb16124143a1f06e1494f2a9cb21d5
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-11-05 18:31:41 -0800

    Remove unused assets Add bootstrap libraries Add Header and styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
+import Header from "./components/Header.jsx";
 
 function App() {
-  return (
-    <h1>Welcome to ProShop</h1>
-  );
+    return (
+        <>
+            <Header />
+            <main className="py-3">
+                <h1>Welcome to ProShop</h1>
+            </main>
+        </>
+    );
 }
 
 export default App;
