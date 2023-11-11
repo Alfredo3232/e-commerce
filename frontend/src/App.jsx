88edbe22719e7eddf6bf5ff8e176ab9fe66f3634
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-11-10 16:14:25 -0800

    Add Product component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,20 @@ import { Container } from "react-bootstrap";
 
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import HomeScreen from "./screens/HomeScreen.jsx";
 
-function App() {
-    return (
-        <>
-            <Header />
+const App = () => (
+    <>
+        <Header />
 
-            <main className="py-3">
-                <Container>
-                    <h1>Welcome to ProShop</h1>
-                </Container>
-            </main>
+        <main className="py-3">
+            <Container>
+                <HomeScreen />
+            </Container>
+        </main>
 
-            <Footer />
-        </>
-    );
-}
+        <Footer />
+    </>
+);
 
 export default App;
---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,20 @@
+import { Row, Col } from "react-bootstrap";
+
+import products from "../products.js";
+
+const HomeScreen = () => (
+    <>
+        <h1>Latest Products</h1>
+        <Row>
+            {
+                products.map((product) => (
+                    <Col sm={12} md={6} lg={4} xl={3}>
+                        <h3>{product.name}</h3>
+                    </Col>
+                ))
+            }
+        </Row>
+    </>
+);
+
+export default HomeScreen;
