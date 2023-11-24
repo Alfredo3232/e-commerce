60a82bf862cb6f5097ae188814f00ec0d8fa46eb
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-11-23 19:53:56 -0800

    Start express app Fix wrong filename Install express

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,23 @@
 import express from "express";
+import products from "./data/products.js";
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("API is Running!");
+});
+app.get("/api/products", (req, res) => {
+    res.send(products);
+});
+app.get("/api/products/:id", (req, res) => {
+    const product = products.find(p => p._id === req.params.id);
+
+    res.send(product);
+});
+
+app.listen(PORT, () => {
+    process.stdout.write(`Server is running at http://localhost:${PORT}\n`);
+});
