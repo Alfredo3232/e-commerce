c6b8d690b3c09390d86a8e722e203d3facf3e056
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-11-23 19:58:01 -0800

    Use env and add comments

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+
 import products from "./data/products.js";
 
+// Start env
+dotenv.config();
+
+// Express instance
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Express plugins
 app.use(express.json());
 
-const PORT = 3000;
-
+// Routes
 app.get("/", (req, res) => {
     res.send("API is Running!");
 });
@@ -18,6 +26,7 @@ app.get("/api/products/:id", (req, res) => {
     res.send(product);
 });
 
+// Startup application
 app.listen(PORT, () => {
     process.stdout.write(`Server is running at http://localhost:${PORT}\n`);
 });
