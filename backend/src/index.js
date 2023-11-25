4840f2f646374cba4cffbf5c209fcb201d5dd6d9
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-11-24 16:23:21 -0800

    Add models for mongo Update how index.js is fired to start app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,34 +1,41 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
 
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
-// Start env
-dotenv.config();
+const start = async () => {
+    await connectDB();
 
-connectDB();
-// Express instance
-const app = express();
-const PORT = process.env.PORT || 3000;
+    // Express instance
+    const app = express();
+    const PORT = process.env.PORT || 3000;
 
-// Express plugins
-app.use(express.json());
+    // Express plugins
+    app.use(express.json());
 
-// Routes
-app.get("/", (req, res) => {
-    res.send("API is Running!");
-});
-app.get("/api/products", (req, res) => {
-    res.send(products);
-});
-app.get("/api/products/:id", (req, res) => {
-    const product = products.find(p => p._id === req.params.id);
+    // Routes
+    app.get("/", (req, res) => {
+        res.send("API is Running!");
+    });
+    app.get("/api/products", (req, res) => {
+        res.send(products);
+    });
+    app.get("/api/products/:id", (req, res) => {
+        const product = products.find(p => p._id === req.params.id);
 
-    res.send(product);
-});
+        res.send(product);
+    });
 
-// Startup application
-app.listen(PORT, () => {
-    process.stdout.write("Running at ---------------> " + `\x1b[3m\x1b[96mhttp://localhost:${PORT}\x1b[39m\x1b[23\x1b[0m` + "\n");
-});
+    // Startup application
+    app.listen(PORT, () => {
+        process.stdout.write("Running at ---------------> " + `\x1b[3m\x1b[96mhttp://localhost:${PORT}\x1b[39m\x1b[23\x1b[0m` + "\n");
+    });
+};
+
+if (import.meta.url === `file://${process.argv [1]}`) {
+    process.stdout.write("\x1b[4mStarting Server\x1b[24m\x1b[0m" + "\n");
+
+    start();
+}
