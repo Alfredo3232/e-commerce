845e302459251f93905d9bc3b4cd70c1435874e5
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-12-06 12:10:33 -0800

    Add getUserProfile logic Add updateUserProfile logic

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -11,7 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
     if (user && (await user.matchPassword(password))) {
         generateToken(res, user._id);
 
-        res.send({
+        res.status(200).send({
             _id     : user._id,
             name    : user.name,
             email   : user.email,
@@ -70,11 +70,42 @@ const logoutUser = asyncHandler(async (req, res) => {
 });
 
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send("get user profile");
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.status(200).send({
+            _id     : user._id,
+            name    : user.name,
+            email   : user.email,
+            isAdmin : user.isAdmin
+        });
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
 });
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.send("update user profile");
+    const user = await User.findById(req.user._id);
+
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        if (req.body.password) user.password = req.body.password;
+
+        const updatedUser = await user.save();
+
+        res.status(200).send({
+            _id     : updatedUser._id,
+            name    : updatedUser.name,
+            email   : updatedUser.email,
+            isAdmin : updatedUser.isAdmin
+        });
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
 });
 
 // Admin Routes
