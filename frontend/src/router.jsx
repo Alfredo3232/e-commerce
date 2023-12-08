a79173c5a547547e9c2415390b2b619c2541b5a8
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-12-07 16:04:18 -0800

    Protect shipping route

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -9,6 +9,8 @@ import App from "./App.jsx";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 
+import PrivateRoute from "./components/PrivateRoute.jsx";
+
 // Pages
 import HomeScreen from "./screens/HomeScreen.jsx";
 import ProductScreen from "./screens/ProductScreen.jsx";
@@ -26,7 +28,10 @@ const router = createBrowserRouter(
             <Route path="/cart" element={<CartScreen />} />
             <Route path="/login" element={<LoginScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
-            <Route path="/shipping" element={<ShippingScreen />} />
+
+            <Route path="" element={<PrivateRoute />} >
+                <Route path="/shipping" element={<ShippingScreen />} />
+            </Route>
         </Route>
     )
 );
