efee2953dbb2b7647b798ec73aa3b084f74b3340
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-12-11 14:09:47 -0800

    Add Profile page

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -21,7 +21,7 @@ import ShippingScreen from "./screens/ShippingScreen.jsx";
 import PaymentScreen from "./screens/PaymentScreen.jsx";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen.jsx";
 import OrderScreen from "./screens/OrderScreen.jsx";
-
+import ProfileScreen from "./screens/ProfileScreen.jsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -37,6 +37,7 @@ const router = createBrowserRouter(
                 <Route path="/payment" element={<PaymentScreen />} />
                 <Route path="/placeorder" element={<PlaceOrderScreen />} />
                 <Route path="/order/:id" element={<OrderScreen />} />
+                <Route path="/profile" element={<ProfileScreen />} />
             </Route>
         </Route>
     )
--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -22,6 +22,13 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 url    : `${USERS_URL}/logout`,
                 method : "POST"
             })
+        }),
+        profile: builder.mutation({
+            query: (data) => ({
+                url    : `${USERS_URL}/profile`,
+                method : "PUT",
+                body   : data
+            })
         })
     })
 });
@@ -29,5 +36,6 @@ export const usersApiSlice = apiSlice.injectEndpoints({
 export const {
     useLoginMutation,
     useLogoutMutation,
-    useRegisterMutation
+    useRegisterMutation,
+    useProfileMutation
 } = usersApiSlice;
