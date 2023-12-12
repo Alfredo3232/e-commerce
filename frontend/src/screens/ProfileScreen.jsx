fe3af952f0c13fc15cde03b7d8dfd4ef11ecc3b7
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-12-11 17:12:00 -0800

    Add Orders in Profile page

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { LinkContainer } from "react-router-bootstrap";
+import { FaTimes } from "react-icons/fa";
 import {
     Table,
     Form,
@@ -14,6 +15,7 @@ import Message from "../components/Message.jsx";
 import Loader from "../components/Loader.jsx";
 import { useProfileMutation } from "../slices/usersApiSlice.js";
 import { setCredentials } from "../slices/authSlice.js";
+import { useGetMyOrdersQuery } from "../slices/ordersApiSlice.js";
 
 
 const ProfileScreen = () => {
@@ -27,6 +29,12 @@ const ProfileScreen = () => {
     const { userInfo } = useSelector((state) => state.auth);
 
     const [updateProfile, { isLoading: loadingUpdateProfile }] = useProfileMutation();
+
+    const {
+        data: orders,
+        isLoading,
+        error
+    } = useGetMyOrdersQuery();
 
     useEffect(() => {
         if (userInfo) {
@@ -107,7 +115,58 @@ const ProfileScreen = () => {
                 {loadingUpdateProfile && <Loader />}
             </Form>
         </Col>
-        <Col md={9}>Column</Col>
+        <Col md={9}>
+            <h2>My Orders</h2>
+
+            {isLoading ? <Loader /> : error ? (
+                <Message variant="danger">
+                    {error?.data?.message || error.error}
+                </Message>
+            ) : (
+                <Table striped hover responsive className="table-sm">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>DATE</th>
+                            <th>TOTAL</th>
+                            <th>PAID</th>
+                            <th>DELIVERED</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.map((order) => (
+                            <tr key={order._id}>
+                                <td>{order._id}</td>
+                                <td>{order.createdAt.substring(0, 10)}</td>
+                                <td>${order.totalPrice}</td>
+                                <td>
+                                    {order.isPaid ? (
+                                        order.paidAt.substring(0, 10)
+                                    ) : (
+                                        <FaTimes style={{ color: "red" }} />
+                                    )}
+                                </td>
+                                <td>
+                                    {order.isDelivered ? (
+                                        order.deliveredAt.substring(0, 10)
+                                    ) : (
+                                        <FaTimes style={{ color: "red" }} />
+                                    )}
+                                </td>
+                                <td>
+                                    <LinkContainer to={`/order/${order._id}`}>
+                                        <Button className="btn-sm" variant="light">
+                                            Details
+                                        </Button>
+                                    </LinkContainer>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            )}
+        </Col>
     </Row>;
 };
 
--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -28,6 +28,12 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
                 url: PAYPAL_URL
             }),
             keepUnusedDataFor: 5
+        }),
+        getMyOrders: builder.query({
+            query: () => ({
+                url: `${ORDERS_URL}/mine`
+            }),
+            keepUnusedDataFor: 5
         })
     })
 });
@@ -36,5 +42,6 @@ export const {
     useCreateOrderMutation,
     useGetOrderDetailsQuery,
     usePayOrderMutation,
-    useGetPayPalClientIdQuery
+    useGetPayPalClientIdQuery,
+    useGetMyOrdersQuery
 } = ordersApiSlice;
