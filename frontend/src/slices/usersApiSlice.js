3d06a3f9dd7e5c63e6325a65a13dcc86dc20d5b9
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-12-12 19:11:58 -0800

    Add UserEditScreen page

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -43,6 +43,20 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 url    : `${USERS_URL}/${userId}`,
                 method : "DELETE"
             })
+        }),
+        getUserDetails: builder.query({
+            query: (userId) => ({
+                url: `${USERS_URL}/${userId}`
+            }),
+            keepUnusedDataFor: 5
+        }),
+        updateUser: builder.mutation({
+            query: (data) => ({
+                url    : `${USERS_URL}/${data.userId}`,
+                method : "PUT",
+                body   : data
+            }),
+            invalidatesTags: ["Users"]
         })
     })
 });
@@ -53,5 +67,7 @@ export const {
     useRegisterMutation,
     useProfileMutation,
     useGetUsersQuery,
-    useDeleteUserMutation
+    useDeleteUserMutation,
+    useGetUserDetailsQuery,
+    useUpdateUserMutation
 } = usersApiSlice;
